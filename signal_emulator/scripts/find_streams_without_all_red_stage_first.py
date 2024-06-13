50f1eb113f7aa19dafefea82b692fc1e52220586
Adam Fradgley <adam.fradgley@tfl.gov.uk>
2024-06-13 16:03:49 +0100

    tidy script

--- signal_emulator/scripts/find_streams_without_all_red_stage_first.py
+++ signal_emulator/scripts/find_streams_without_all_red_stage_first.py
@@ -2,25 +2,12 @@ from signal_emulator.emulator import SignalEmulator
 from signal_emulator.utilities.utility_functions import load_json_to_dict
 
 
-def run_all(config_path):
+def run(config_path):
     config = load_json_to_dict(json_file_path=config_path)
     signal_emulator = SignalEmulator(config=config)
     codes = signal_emulator.find_streams_without_all_red_stage_first()
     print(codes)
-    # signal_emulator.generate_signal_plans(config.get("ped_only", False))
-    # signal_emulator.generate_phase_timings()
-    # signal_emulator.generate_visum_signal_groups()
-    # signal_emulator.generate_saturn_signal_groups()
-    # signal_emulator.saturn_signal_groups.export_to_rgs_files()
-    # signal_emulator.visum_signal_controllers.export_all_to_net_files()
-    # signal_emulator.visum_signal_groups.export_all_to_net_files()
-    # signal_emulator.linsig.export_all_to_lsg_v236()
-    # signal_emulator.export_to_database(config.get("output_schema", None))
-
-
-def run():
-    run_all(config_path="signal_emulator/resources/configs/signal_emulator_from_files_config.json")
 
 
 if __name__ == "__main__":
-    run()
+    run(config_path="signal_emulator/resources/configs/signal_emulator_from_files_config.json")
